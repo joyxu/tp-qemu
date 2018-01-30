0c4c6b8c929bc3fbd39e009087c568d156461818
tanshukun <tanshukun1@huawei.com>
2018-01-30 03:35:56 +0000

    use try...except...finally it is better to use this structure otherwise may influence other tests

--- qemu/tests/migration_after_nichotplug.py
+++ qemu/tests/migration_after_nichotplug.py
@@ -49,72 +49,80 @@ def run(test, params, env):
     mig_cancel_delay = int(params.get("mig_cancel") == "yes") * 2
     pci_model = params.get("pci_model")
     nic_extra_params = ",bus=root_port0"
+    clonevm = None
 
-    # Modprobe the module if specified in config file
-    module = params.get("modprobe_module")
-    if guest_is_not_windows and module:
-        session_serial.cmd("modprobe %s" % module)
-    if session_serial:
-        session_serial.close()
-
-    error.context("Add network devices through monitor cmd", logging.info)
-    nic_name = 'hotadded'
-    enable_msix_vectors = params.get("enable_msix_vectors")
-    nic_info = vm.hotplug_nic(nic_model=pci_model, nic_name=nic_name,
-                              netdst=netdst, nettype=nettype,
-                              queues=params.get('queues'),
-                              enable_msix_vectors=enable_msix_vectors,
-                              nic_extra_params=nic_extra_params)
-    nic_mac = nic_info['mac']
-    vm.params['nics'] += " %s" % nic_name
-    vm.params['nic_model_%s' % nic_name] = nic_info['nic_model']
-
-    # Only run dhclient if explicitly set and guest is not running Windows.
-    # Most modern Linux guests run NetworkManager, and thus do not need this.
-    if run_dhclient == "yes" and guest_is_not_windows:
-        session_serial = vm.wait_for_serial_login(timeout=timeout)
-        ifname = utils_net.get_linux_ifname(session_serial, nic_mac)
-        utils_net.restart_guest_network(session_serial, ifname)
-        # Guest need to take quite a long time to set the ip addr, sleep a
-        # while to wait for guest done.
-        time.sleep(60)
-
-    error.context("Disable the primary link of guest", logging.info)
-    set_link(nic_name, up=False)
-
-    error.context("Check if new interface gets ip address", logging.info)
     try:
-        ip = vm.wait_for_get_address(nic_name)
-    except virt_vm.VMIPAddressMissingError:
-        raise error.TestFail("Could not get or verify ip address of nic")
-    logging.info("Got the ip address of new nic: %s", ip)
+	# Modprobe the module if specified in config file
+	module = params.get("modprobe_module")
+	if guest_is_not_windows and module:
+	    session_serial.cmd("modprobe %s" % module)
+	if session_serial:
+	    session_serial.close()
 
-    error.context("Ping guest's new ip from host", logging.info)
-    s, o = utils_test.ping(ip, 10, timeout=15)
-    if s != 0:
-        raise error.TestFail("New nic failed ping test with output:\n %s" % o)
+	error.context("Add network devices through monitor cmd", logging.info)
+	nic_name = 'hotadded'
+	enable_msix_vectors = params.get("enable_msix_vectors")
+	nic_info = vm.hotplug_nic(nic_model=pci_model, nic_name=nic_name,
+			      netdst=netdst, nettype=nettype,
+			      queues=params.get('queues'),
+			      enable_msix_vectors=enable_msix_vectors,
+			      nic_extra_params=nic_extra_params)
+	nic_mac = nic_info['mac']
+	vm.params['nics'] += " %s" % nic_name
+	vm.params['nic_model_%s' % nic_name] = nic_info['nic_model']
 
-    error.context("Re-enabling the primary link", logging.info)
-    set_link(nic_name, up=True)
+	# Only run dhclient if explicitly set and guest is not running Windows.
+	# Most modern Linux guests run NetworkManager, and thus do not need this.
+	if run_dhclient == "yes" and guest_is_not_windows:
+	session_serial = vm.wait_for_serial_login(timeout=timeout)
+	ifname = utils_net.get_linux_ifname(session_serial, nic_mac)
+	utils_net.restart_guest_network(session_serial, ifname)
+	    # Guest need to take quite a long time to set the ip addr, sleep a
+	    # while to wait for guest done.
+	    time.sleep(60)
 
-    error.context("Migrate from source VM to Destination VM", logging.info)
-    vm.migrate(mig_timeout, mig_protocol, mig_cancel_delay, env=env)
+	error.context("Disable the primary link of guest", logging.info)
+	set_link(nic_name, up=False)
 
-    error.context("Disable the primary link", logging.info)
-    set_link(nic_name, up=False)
+	error.context("Check if new interface gets ip address", logging.info)
+	try:
+	    ip = vm.wait_for_get_address(nic_name)
+	except virt_vm.VMIPAddressMissingError:
+	    raise error.TestFail("Could not get or verify ip address of nic")
+	logging.info("Got the ip address of new nic: %s", ip)
 
-    error.context("Ping guest's new ip from host", logging.info)
-    s, o = utils_test.ping(ip, 10, timeout=15)
-    if s != 0:
-        raise error.TestFail("New nic failed ping test with output:\n %s" % o)
+	error.context("Ping guest's new ip from host", logging.info)
+	s, o = utils_test.ping(ip, 10, timeout=15)
+	if s != 0:
+	    raise error.TestFail("New nic failed ping test with output:\n %s" % o)
 
-    error.context("Re-enabling the primary link", logging.info)
-    set_link(nic_name, up=True)
+	error.context("Re-enabling the primary link", logging.info)
+	set_link(nic_name, up=True)
 
-    error.context("Reboot guest and verify new nic works", logging.info)
-    host_ip = utils_net.get_ip_address_by_interface(netdst)
-    session = vm.reboot()
-    status, output = utils_test.ping(dest=host_ip, count=100,
-                                     timeout=240, session=session)
-    if status != 0:
-        raise error.TestFail("Fail to ping host form guest")
+	error.context("Migrate from source VM to Destination VM", logging.info)
+	clonevm = vm.migrate(mig_timeout, mig_protocol, mig_cancel_delay, env=env)
+
+	error.context("Disable the primary link", logging.info)
+	set_link(nic_name, up=False)
+
+	error.context("Ping guest's new ip from host", logging.info)
+	s, o = utils_test.ping(ip, 10, timeout=15)
+	if s != 0:
+	    raise error.TestFail("New nic failed ping test with output:\n %s" % o)
+
+	error.context("Re-enabling the primary link", logging.info)
+	set_link(nic_name, up=True)
+
+	error.context("Reboot guest and verify new nic works", logging.info)
+	host_ip = utils_net.get_ip_address_by_interface(netdst)
+	session = vm.reboot()
+	status, output = utils_test.ping(dest=host_ip, count=100,
+				     timeout=240, session=session)
+	if status != 0:
+	    raise error.TestFail("Fail to ping host form guest")
+
+    finally:
+	if clonevm:
+	    clonevm.destroy(gracefully=False)
+	if vm:
+	    vm.destroy(gracefully=False)
